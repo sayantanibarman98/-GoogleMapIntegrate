8b01308c5d92407d441e28f7fb0f3b211e7aeebb
Sayantani Barman <48579184+sayantanibarman98@users.noreply.github.com>
2020-10-03 09:28:41 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.transpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment supportMapFragment=(SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+        LatLng Kolkata=new LatLng(22.466600, 88.102629);
+        map.addMarker(new MarkerOptions().position(Kolkata).title("Kolkata"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Kolkata));
+
+    }
+}
